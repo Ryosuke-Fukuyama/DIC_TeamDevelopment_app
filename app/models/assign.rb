4401a164ec24cc19b295600dd62091f22913ee0b
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2021-05-24 17:04:42 +0900

    Teamに所属しているUserの削除（離脱）は、そのTeamのオーナーか、そのUser自身しかできないようにするTeamのeditはTeamのリーダー（オーナー）のみができるようにする

--- app/models/assign.rb
+++ app/models/assign.rb
@@ -1,4 +1,16 @@
 class Assign < ApplicationRecord
   belongs_to :user
   belongs_to :team
+
+  # before_destroy :owner_or_current
+
+
+  private
+
+  # def owner_or_current
+  #   user = User.find(id)
+  #   unless user == owner || current_user
+  #     throw(:abort)
+  #   end
+  # end
 end
