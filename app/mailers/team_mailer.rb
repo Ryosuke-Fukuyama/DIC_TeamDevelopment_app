ef98bfcc6a5945eb33cb82ad6155beaae9c26194
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2021-06-02 10:34:21 +0900

    agenda_del_mail

--- app/mailers/team_mailer.rb
+++ app/mailers/team_mailer.rb
@@ -2,6 +2,12 @@ class TeamMailer < ApplicationMailer
 
 
 
-  def agenda_del_mail
+
+
+
+  def agenda_del_mail(team, users)
+    @team = team
+    @users = users
+    mail to: "ryosukemakaritoru425@gmail.com", subject: "アジェンダが削除されました！"
   end
 end
